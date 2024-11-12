83ea1c2f3737c979e7ef0c907c32b2bce5bb0570
AnthonySaimon <22202609@aluno.cotemig.com.br>
2024-11-11 22:44:33 -0300

    remodelasao_1

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Page Title</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='login.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='Login.css'>
     <script src='main.js'></script>
 </head>
 
@@ -15,34 +15,38 @@
         <div class="panel">
             <h2>Login</h2>
             <form method="POST" action="login.php">
-                <label for="username">Email:</label>
-                <input type="text" id="username" name="username" required>
-
-                <label for="password">Senha:</label>
-                <input type="password" id="password" name="password" required>
-
-                <button class="logar1" type="submit">Logar</button>
-            </form>
-            <div class="extra">
-                <p>Não possui uma conta? <a class="link" href="./cadastrar/cadastro.html">Cadastrar</a></p>
-                
-
-                <div class="incorreto">
-                    <br>
-                    <?php
-                    // Mostra mensagem de erro, se houver
-                    if (isset($_GET['erro'])) {
-                        $erro = $_GET['erro'];
-                        if ($erro == 1) {
-                            echo  '<h3>Senha incorreta.</h3>';
-                        } elseif ($erro == 2) {
-                            echo '<h3>Email não encontrado.</h3>';
-                        }
-                    }
-                    ?>
+                <div class="bodas">
+                    <label for="username">Email:</label>
+                    <input type="text" id="username" name="username" required>
                 </div>
 
+                <div class="bodas">
+                    <label for="password">Senha:</label>
+                    <input type="password" id="password" name="password" required>
+                </div>
 
+                <br>
+                <button class="logar1" type="submit">Logar</button>
+                <a class="logar2" href="./cadastrar/cadastro.html">Cadastrar</a>
+            </form>
+            <div class="extra">
+                <?php
+                // Mostra mensagem de erro, se houver
+                if (isset($_GET['erro'])) {
+                    $erro = $_GET['erro'];
+                    if ($erro == 1) {
+                        echo  '<div class="incorreto">
+                                   <br>
+                                   <h4>Senha incorreta.</h4>
+                                   </div>';
+                    } elseif ($erro == 2) {
+                        echo '<div class="incorreto">
+                               <br>
+                               <h4>Email não encontrado.</h4>
+                               </div>';
+                    }
+                }
+                ?>
             </div>
         </div>
     </div>
